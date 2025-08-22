a8de3e1a3b182aedc2489cafcff710fb556a23f6
nhatcoi <nhatcoifacebook@gmail.com>
2025-08-22 15:27:51 +0700

    add authentication dto

---
+++ src/main/java/com/nhatcoi/security/auth/dto/request/AuthenticationRequest.java
@@ -0,0 +1,22 @@
+package com.nhatcoi.security.auth.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+    @Email(message = "Email không hợp lệ")
+    @NotBlank(message = "Email không được để trống")
+    private String email;
+
+    @NotBlank(message = "Mật khẩu không được để trống")
+    private String password;
+}
